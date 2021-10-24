5660777e1e55e43040b647ef57e65a090688dbef
Michael Peng <michael@mcmoo.org>
2021-10-24 12:08:19 -0400

    Add vacc and parties data adapter

---
+++ data/politics.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+raw_df = pd.read_csv('https://raw.githubusercontent.com/Casper-Guo/MDST-Covid-Vaccination/main/state-political-identification.csv')
+
+state_parties = raw_df \
+    .rename({'Unnamed: 0': 'State'}, axis=1) \
+    .drop(['Unnamed: 3', 'Unnamed: 4'], axis=1) \
+    .dropna()
