fdad33362b26a458e3eb7facf881de17b60a73c2
Casper-Guo <89810860+Casper-Guo@users.noreply.github.com>
2021-10-31 12:08:14 -0400

    Update education.py

--- data/education.py
+++ data/education.py
@@ -3,5 +3,3 @@ import pandas as pd
 
 link = "https://raw.githubusercontent.com/Casper-Guo/MDST-Covid-Vaccination/main/education_clean.csv"
 state_education = pd.read_csv(link)
-
-print(state_education.head())
