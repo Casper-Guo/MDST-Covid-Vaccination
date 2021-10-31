acedb3f8f7e5727d6cbcfe9c9c044a6c59485900
Casper-Guo <89810860+Casper-Guo@users.noreply.github.com>
2021-10-31 12:58:09 -0400

    Update medianIncome.py

--- data/medianIncome.py
+++ data/medianIncome.py
@@ -3,3 +3,5 @@ import pandas as pd
 
 link = "https://raw.githubusercontent.com/Casper-Guo/MDST-Covid-Vaccination/main/medianIncomeByState2019.csv"
 state_median_income = pd.read_csv(link)
+
+state_median_income["Median Annual Household Income"] = state_median_income["Median Annual Household Income"].map(lambda x:int(x[1:]))
