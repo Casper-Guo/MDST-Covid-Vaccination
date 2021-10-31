e2f50290226e80f0bffea1b8ff39c6223eeface2
Michael Peng <michael@mcmoo.org>
2021-10-31 11:51:31 -0400

    Add data.mask_usage, adjust for 'STATE' convention

---
+++ data/mask_usage.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+raw_df = pd.read_csv('https://raw.githubusercontent.com/Casper-Guo/MDST-Covid-Vaccination/main/covid_mask_usage_by_state.csv')
+state_masks = raw_df.rename(columns={'State': 'STATE'})
--- data/vaccinations.py
+++ data/vaccinations.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
 raw_df = pd.read_csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/us_state_vaccinations.csv")
-state_vacc = raw_df.dropna()
+state_vacc = raw_df.dropna().rename(columns={'location': 'STATE'})
